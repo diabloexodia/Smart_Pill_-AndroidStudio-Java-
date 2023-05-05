ef8ae503d1fafec211819ed33a0a85b2ef46ad2a
unknown <diaabloexodia@gmail.com>
2023-05-05 17:19:14 +0530

    Added Firebase Authentication

--- AndroidStudioProjects/SMARTPILLDISPENSER/app/src/main/java/com/example/smartpilldispenser/MainActivity.java
+++ AndroidStudioProjects/SMARTPILLDISPENSER/app/src/main/java/com/example/smartpilldispenser/MainActivity.java
@@ -1,70 +1,41 @@
 package com.example.smartpilldispenser;
 
 
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
-
-import com.example.smartpilldispenser.Pill;
-import com.example.smartpilldispenser.Human;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActionBar;
-import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.PopupMenu;
 import android.widget.Spinner;
 import android.widget.TimePicker;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
-import com.amazonaws.auth.CognitoCachingCredentialsProvider;
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Region;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
-
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.PutObjectResult;
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
-import android.os.Bundle;
-import android.util.Log;
-
+import com.google.firebase.auth.FirebaseAuth;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
-
 
 public class MainActivity extends AppCompatActivity {
     private EditText timingEditText;
     static private String selecteddosageValue;
-    // List<String> pillTimingArrayList;
+
     private EditText naemEditText;
     private Spinner dosageEdittext;
     private Button pillListadder;
@@ -74,12 +45,8 @@ public class MainActivity extends AppCompatActivity {
     private Button clearButton;
     private static final String BUCKET_NAME = "smartpills";
     private static final String OBJECT_KEY = "userdetails/userdetails.txt";
-
     private AmazonS3Client s3Client;
-
     Human human = null;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -90,25 +57,21 @@ public class MainActivity extends AppCompatActivity {
         clearButton = findViewById(R.id.clear);
         pillIdedittext = findViewById(R.id.pillId);
         dosageEdittext = findViewById(R.id.dosageSpinner);
-        //  pillTimingArrayList=new ArrayList<>();
         pillListadder = findViewById(R.id.pillAdd2);
         naemEditText = findViewById(R.id.name);
 
         BasicAWSCredentials credentials = new BasicAWSCredentials("AKIAWZE4LHRMMINZAQYH", "/J0o48inVABsBgAvHSS6jQ0rfslq72hpZYTd7sAi");
         s3Client = new AmazonS3Client(credentials);
         s3Client.setRegion(Region.getRegion(Regions.AP_SOUTHEAST_1));
-        // Get the Spinner view by its id
 
         dosageEdittext.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                selecteddosageValue = parent.getItemAtPosition(position).toString(); // Get the selected item and convert it to a String
-                // Do something with the current value, such as update a TextView or send it to a server
+                selecteddosageValue = parent.getItemAtPosition(position).toString();
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                // Do nothing
+
             }
         });
 
@@ -116,8 +79,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 timingEditText = findViewById(R.id.timeEditText);
-
-
                 pillIdedittext = findViewById(R.id.pillId);
                 if (!naemEditText.getText().toString().isEmpty()) naemEditText.setText(" ");
                 if (!timingEditText.getText().toString().isEmpty()) timingEditText.setText(" ");
@@ -143,8 +104,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 PopupMenu popupMenu = new PopupMenu(MainActivity.this, view);
                 popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
-
-                // Set an OnMenuItemClickListener for the menu items
                 popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     @Override
                     public boolean onMenuItemClick(MenuItem menuItem) {
@@ -152,8 +111,6 @@ public class MainActivity extends AppCompatActivity {
                         return true;
                     }
                 });
-
-                // Show the pop-up menu
                 popupMenu.show();
             }
         });
@@ -179,8 +136,6 @@ public class MainActivity extends AppCompatActivity {
                         timingEditText.setText(newtime);
                         if (human == null) {
                             human = new Human(naemEditText.getText().toString());
-//                            selecteddosageValue=dosageEdittext.toString();
-//                            Log.d("broo",selecteddosageValue);
                             human.setPillObject(pillIdedittext.getText().toString(), Integer.parseInt(selecteddosageValue));
                         }
 
@@ -193,16 +148,12 @@ public class MainActivity extends AppCompatActivity {
         pillListadder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //  String TimePickerTime = timingEditText.getText().toString();
-                // empty edge case
-//                Log.d("brogh",naemEditText.getText().toString());
                 if (timingEditText == null || pillIdedittext == null || naemEditText == null) {
                     Toast.makeText(MainActivity.this, "Please input other details first !", Toast.LENGTH_SHORT).show();
                 } else if (!human.pillObject.time.contains(timingEditText.getText().toString())) {
 
                     human.pillObject.time.add(timingEditText.getText().toString());
-//                    Toast.makeText(MainActivity.this, "Time Added", Toast.LENGTH_SHORT).show();
-                    Toast.makeText(MainActivity.this, human.getHumanName()+ " " + String.valueOf(human.pillObject.getName()) + " " + human.pillObject.time.get(human.pillObject.time.size() - 1), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, human.pillObject.time.get(human.pillObject.time.size() - 1) +" added", Toast.LENGTH_SHORT).show();
 
                 } else
                     Toast.makeText(MainActivity.this, "Time already exists !", Toast.LENGTH_SHORT).show();
@@ -226,34 +177,16 @@ public class MainActivity extends AppCompatActivity {
             pillIdedittext = findViewById(R.id.pillId);
 
 
-            // Get the current time
-
-
 //----------------------------------------OOP-----------------------------------------
-//            Human human = new Human(naemEditText.getText().toString());
-
-
             String allTimings = "";
-//            for(String s:pillTimingArrayList) {
-//                human.pillObject.setTime(s);
-//                allTimings+=s;
-//                allTimings+=",";
-//
-//            }
 
             for (String s : human.pillObject.time) {
-//                human.pillObject.setTime(s);
-//                if(s!=null) {
                 allTimings += s;
                 allTimings += ",";
-//                }
             }
-
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder.append("Name: ").append(human.getHumanName()).append("\n").append("Time: ").append(allTimings).append("\n").append("Pill Name: ").append(human.pillObject.getName()).append("\n").append("Dosage: ").append(human.pillObject.getPillDosage());
-
             String formattedString = stringBuilder.toString();
-
 
 //-------------------------------------------------------------------------------------
 
@@ -289,6 +222,27 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(MainActivity.this, "Pill Confguration Stored", Toast.LENGTH_SHORT).show();
             human = null;
         }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.logout) {
+            // Handle the settings item
+            FirebaseAuth mAuth = FirebaseAuth.getInstance();
+            mAuth.signOut();
+            Toast.makeText(this, "Logged out Successfully", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
